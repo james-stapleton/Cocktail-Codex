52308199d5c7eaed61144762266f064de0ab69d7
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-08-29 12:00:58 -0400

    added temp by city code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-// 
+var apiKey = "6015d4614214e35f89f83b4825650637";
+var currentWeatherEl = document.querySelector("#current-weather");
+var currentCityEl = document.querySelector("#current-city");
+var currentWeatherIconEl = document.querySelector("#current-weather-icon")
+var currentTempEl = document.querySelector("#current-temp");
+var searchFormEl = document.querySelector('#search-form');
+
+function getCurrentWeather(city) {
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + "&appid=" + apiKey + "&units=imperial")
+
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (response) {
+            var cityName = response.name;
+            var currentDate = moment().format("M/D/YYYY");
+            var currentTemp = response.main.temp;
+
+            currentTempEl.textContent = "Temp: " + Math.round(currentTemp) + " \u00B0F";
+        })
+};
+
+function handleSearchFormSubmit(e) {
+    e.preventDefault();
+    var searchInputVal = document.querySelector('#search-input').value;
+
+    if (!searchInputVal) {
+        console.log("Invalid Input!");
+        return;
+    }
+    getCurrentWeather(searchInputVal);
+
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
