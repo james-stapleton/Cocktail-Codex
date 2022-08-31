494b4612c59105b46e9cd6b001b0361e9e350833
James Stapleton <jstapleton536@gmail.com>
2022-08-31 11:12:26 -0400

    local storage logic with save button dynmaic generation

--- assets/js/savedDrinks.js
+++ assets/js/savedDrinks.js
@@ -1,6 +1,29 @@
 console.log("This file is linked!");
 var savedDrinksButton = document.querySelector("#save-btn");
-savedDrinksButton.addEventListener("click", function() {
-    location.href="./savedDrinks.html";
-    return;
-});
+
+var savedList = document.querySelector("#saved-drinks");
+
+localStorage.setItem("drinks", JSON.stringify(["moscow_mule", "tom_collins", "rum_punch", "sazerac", "martini", "whiskey_sour", "sidecar"]));
+
+var savedDrinks = localStorage.getItem("drinks");
+var savedDrinksArray;
+if (!savedDrinks) {
+    savedDrinksArray = [];
+}
+else {
+    savedDrinksArray = JSON.parse(localStorage.getItem("drinks"));
+}
+
+for (var i = 0; i<savedDrinksArray.length; i++) {
+    var list = document.createElement("li");
+    list.textContent = savedDrinksArray[i];
+    savedList.appendChild(list);
+}
+
+
+var save = document.createElement("button");
+save.textContent = "Save Drink";
+savedList.appendChild(save);
+save.addEventListener("click", function () {
+    
+})
