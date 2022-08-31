1cb6742f51f2c5201852fed3b121672575f0479c
James Stapleton <jstapleton536@gmail.com>
2022-08-31 16:13:25 -0400

    Event delegation for list and images to open recipes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,18 @@ var recipeParent = document.querySelector("#recipe");
 var savedList = document.querySelector("#saved-drinks");
 var tempText = document.querySelector("#tempText")
 
-//!-------------------------------------------------------------------
-//! Create Save Button and local storage logic
+
+// Event delegation function to display a recipe when a saved drink is clicked on
+savedList.addEventListener("click", function (event) {
+    var targetDrink = event.target; 
+    var targetDrinkName = targetDrink.textContent;
+    console.log(targetDrinkName);
+    var drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+targetDrinkName;
+    tempSearch(drinkURL);
+});
+
 // localStorage.setItem("drinks", JSON.stringify(["moscow_mule", "tom_collins", "rum_punch", "sazerac", "martini", "whiskey_sour", "sidecar"]));
-// localStorage.clear();
+localStorage.clear();
 var savedDrinks = localStorage.getItem("drinks");
 var savedDrinksArray = [];
 if (!savedDrinks) {
@@ -37,10 +45,6 @@ function generateSaveButton () {
 
     recipeParent.appendChild(save);
 }
-//--------------------------------------------------------------
-
-//!-----------------------------------------------------------------
-//! Local storage to load saved items
 
 var savedDrinksButton = document.querySelector("#saved-drinks-btn");
 savedDrinksButton.addEventListener("click", function () {
@@ -53,12 +57,6 @@ savedDrinksButton.addEventListener("click", function () {
         savedList.appendChild(list);
     }
 });
-
-
-
-//!--------------------------------------------------------------------
-
-
 
 function getCurrentWeather(city) {
 
@@ -122,11 +120,27 @@ function getCocktailFromIngredient(ingredientName) {
             var drinkName = document.createElement("li");
             drinkName.textContent = data[i].strDrink;
             var image = document.createElement("img");
+            image.id="image"+i;
+            //!----------------------------------------------------------------------------
+
+           image.addEventListener("click", function (event) {
+            var imageTarget = event.target;
+
+            var imageID = imageTarget.id;
+            var imageName = document.getElementById(imageID).previousSibling.textContent;
+            console.log(imageName);
+            var drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+imageName;
+            tempSearch(drinkURL);
+            generateSaveButton();
+            console.log(imageID);
+             
+         })
+ 
+         //!----------------------------------------------------------------------------
             image.src = data[i].strDrinkThumb;
             searchedParent.appendChild(drinkName);
             drinkName.appendChild(image);
         }
-
     }
 
 //!----------------------------------------
@@ -257,7 +271,7 @@ function toString(data) {
     cityDrink.innerHTML = "";
     var recipeName = document.createElement("h3");
     recipeName.id="recipe-name";
-    nameVar = (data.drinks[0].strDrink + " recipe:");
+    nameVar = (data.drinks[0].strDrink);
     recipeName.textContent = nameVar;
     //!Dynamically create save button with drink name"
     
