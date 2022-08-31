9ae98aa66013d9229dfb5b41b28c9bd3168b237c
FatihSul <fatih9720@hotmail.com>
2022-08-31 13:58:40 -0400

    added text to display based on tempurature for suggestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ var cityDrink = document.querySelector("#city-drink-name")
 var currentTemp;
 var recipeParent = document.querySelector("#recipe");
 var savedList = document.querySelector("#saved-drinks");
+var tempText = document.querySelector("#tempText")
 
 //!-------------------------------------------------------------------
 //! Create Save Button and local storage logic
@@ -158,6 +159,7 @@ function drinkRec(currentTemp) {
     var drink = "";//empty string that will be given the name of a drink from the array
     var drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="; //URL for the fetch that will be appended with drink string
 // conditional code for temperature ranges
+    tempText.innerHTML = "";
 if(currentTemp > 85) {
     //populate the array
     drinksArray = ["margarita", "mojito", "aperol_spritz", "pina_colada", "daiquiri", "paloma", "white_wine_sangria", "mint_julep"];
@@ -168,6 +170,9 @@ if(currentTemp > 85) {
     drinkURL += drink;
     tempSearch(drinkURL);
     console.log("It's a scorcher today! Try a refreshing " + drink);
+    var weatherDrink = document.createElement("h2")
+    weatherDrink.textContent = ("It's a scorcher today! Try a refreshing " + drink);
+    tempText.appendChild(weatherDrink);
     }
 
 else if (currentTemp > 60 && currentTemp <=85) {
@@ -177,6 +182,9 @@ else if (currentTemp > 60 && currentTemp <=85) {
     drinkURL += drink;
     tempSearch(drinkURL);
     console.log("Nice Day! Relax with an easy-sipping " + drink);
+    var weatherDrink = document.createElement("h2")
+    weatherDrink.textContent = ("Nice Day! Relax with an easy-sipping " + drink);
+    tempText.appendChild(weatherDrink);
     }
 
 else if (currentTemp >40 && currentTemp <= 60) {
@@ -186,6 +194,9 @@ else if (currentTemp >40 && currentTemp <= 60) {
     drinkURL += drink;
     tempSearch(drinkURL);
     console.log("It's a bit chilly! You need a little liquid warmth from a stiff " + drink);
+    var weatherDrink = document.createElement("h2")
+    weatherDrink.textContent = ("It's a bit chilly! You need a little liquid warmth from a stiff " + drink);
+    tempText.appendChild(weatherDrink);
     }
 
 else {
@@ -195,6 +206,9 @@ else {
      drinkURL += drink;
      tempSearch(drinkURL);
      console.log("Brr. Warm up with a " + drink);
+     var weatherDrink = document.createElement("h2")
+     weatherDrink.textContent = ("Brr. Warm up with a " + drink);
+     tempText.appendChild(weatherDrink);
     }
 }
 
@@ -241,7 +255,7 @@ function toString(data) {
     cityDrink.innerHTML = "";
     var recipeName = document.createElement("h3");
     recipeName.id="recipe-name";
-    nameVar = data.drinks[0].strDrink;
+    nameVar = (data.drinks[0].strDrink + " recipe:");
     recipeName.textContent = nameVar;
     //!Dynamically create save button with drink name"
     
