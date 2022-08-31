571010257313a47ca8688633629b0c2aacc748d3
James Stapleton <jstapleton536@gmail.com>
2022-08-31 10:21:44 -0400

    Created button for saved recipes that links to new html page

---
+++ assets/js/savedDrinks.js
@@ -0,0 +1,6 @@
+console.log("This file is linked!");
+var savedDrinksButton = document.querySelector("#save-btn");
+savedDrinksButton.addEventListener("click", function() {
+    location.href="./savedDrinks.html";
+    return;
+});
